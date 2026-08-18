5147dd3dd8c3bdaa94e8eccd081fcf745d8e243c
David <davidalejandroperezpuerto@gmail.com>
2026-08-18 00:00:00 -0400

    prueba #4 fix2.0 del admin panel

--- db/models/producto.py
+++ db/models/producto.py
@@ -4,7 +4,7 @@ from typing import Optional
 class Product(BaseModel):
     id : str | None = None
     name : str
-    precio : int
+    precio : float
     categoria : str
     imagen : str
     descripcion : str
