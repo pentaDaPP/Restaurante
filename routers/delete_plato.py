5498dfc533ecaabfc3ede07ddfea7f81831c0b00
David <davidalejandroperezpuerto@gmail.com>
2026-08-20 01:45:21 -0400

    funcion eliminar desde el admin.html v2.4

--- routers/delete_plato.py
+++ routers/delete_plato.py
@@ -12,7 +12,7 @@ router = APIRouter(
                 responses={404:{"message":"no encontrado"}}
 )
 
-@router.delete("/{id}",status_code=201)
+@router.delete("/{id}",status_code=200)
 def eliminar_plato(id : str , api_key : str = Depends(verificar_api_key)):
     plato_id = db["platos"].find_one({"_id" : ObjectId(id)})
     
@@ -21,7 +21,7 @@ def eliminar_plato(id : str , api_key : str = Depends(verificar_api_key)):
 
     #eliminar de cloudinary
     if plato_id.get("imagen_id"):
-        cloudinary.uploader.destroy(plato_id)
+        cloudinary.uploader.destroy(plato_id["imagen_id"])
         
     #eliminar plato de mongodb
     resultado = db["platos"].delete_one({"_id" : ObjectId(id)})
@@ -29,5 +29,5 @@ def eliminar_plato(id : str , api_key : str = Depends(verificar_api_key)):
     if resultado.deleted_count ==0:
         raise HTTPException(status_code=500,detail="No se pudo eliminar el plato")
         
-    return "Plato eliminado correctamente"
+    return {"message":"Plato eliminado correctamente"}
     
